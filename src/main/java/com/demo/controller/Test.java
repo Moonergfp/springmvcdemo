cb4896ef727a88cd37972c48a0bcc99de54006e9
guofeipeng <guofeipeng@meituan.com>
2016-01-09 23:52:24 +0800

    save

--- src/main/java/com/demo/controller/Test.java
+++ src/main/java/com/demo/controller/Test.java
@@ -1,14 +1,15 @@
-package com.demo.sitemesh.controller;
+package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by guofeipeng on 15/11/23.
+ * Created by guofeipeng on 16/1/9.
  */
 @Controller
 @RequestMapping("/test")
-public class TestController {
+public class Test {
+
     @RequestMapping("/method")
     public String method(){
         return "index";
